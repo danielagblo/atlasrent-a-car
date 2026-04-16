e8e707c002ee9682462a3181dc9ae1c100acd040
= <=>
2026-04-16 01:21:16 +0000

    feat: implement custom app shell with global layout components and site metadata icons

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -17,9 +17,10 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Atlas Rent-A-Car | Luxury Vehicle Rentals & Private Mobility</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#071029" />
       </Head>
       <a className="skip-link" href="#site-main">Skip to content</a>
