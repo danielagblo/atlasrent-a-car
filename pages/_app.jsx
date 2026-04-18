83286ddf83eefa653825ef4450e343c84afdb24e
= <=>
2026-04-18 17:38:39 +0000

    feat: initialize custom App component with global styles, SEO metadata, and conditional layout rendering

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -20,7 +20,7 @@ export default function App({ Component, pageProps }) {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <link rel="manifest" href="/manifest.webmanifest" />
         <meta name="theme-color" content="#071029" />
       </Head>
